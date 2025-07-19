16df05860f6404466b1395ec99f84147ff43a97a
Mayuresh Teli <mayureshteli25@gmail.com>
2025-07-20 01:46:33 +0530

    grade calculator.java

--- grade calculator.java
+++ grade calculator.java
@@ -3,17 +3,17 @@ class grade {
     public static void main(String[] args) {
         System.out.println("ENTER YOUR MARKS ");
         Scanner md = new Scanner(System.in);
-        System.out.println("ENTER YOUR MARKS IN ENGILSH : ");
+        System.out.print("ENTER YOUR MARKS IN ENGILSH : ");
         int a = md.nextInt();
-        System.out.println("ENTER YOUR MARKS IN MATHS : ");
+        System.out.print("ENTER YOUR MARKS IN MATHS : ");
         int b = md.nextInt();
-        System.out.println("ENTER YOUR MARKS IN SCIENCE : ");
+        System.out.print("ENTER YOUR MARKS IN SCIENCE : ");
         int c = md.nextInt();
         int SUM = a+b+c;
         float percent=SUM/3f;
         System.out.println("*****REPORT CARD *****");
         System.out.println("YOUR TOTAL MARKS IS :"+SUM);
-        System.out.println("YOUR PERCENTAGE IS :"+percent);
+        System.out.print("YOUR PERCENTAGE IS :"+percent);
         md.close();
 
 
